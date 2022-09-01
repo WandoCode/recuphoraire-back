5a86e7b22dc3225b51525dd3be5cad5fe01c6e6b
Wandole <mchirez@gmail.com>
2022-09-01 19:00:10 +0300

    Hreoku deployment

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const cors = require('cors')
 
 require('dotenv').config()
 
-let corsOptions = {
-  origin: [process.env.CORS_URL],
-}
+// let corsOptions = {
+//   origin: [process.env.CORS_URL],
+// }
 
 var sheetsRouter = require('./routes/sheets')
 
@@ -21,7 +21,8 @@ var app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
