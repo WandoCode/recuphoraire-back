c056cf591dc1af9c8286ce4633bff03e38a711ca
Wandole <mchirez@gmail.com>
2022-09-18 13:46:06 +0200

    Send email when connexion successfull

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,8 +1,33 @@
+let nodemailer = require('nodemailer')
+
 exports.login = (req, res, next) => {
   const username = req.body.username
   const password = req.body.password
 
   if (username === 'techdel' && password === 'chirec') {
+    console.log(process.env.EMAIL_PASSWORD)
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    })
+    let mailOptions = {
+      from: process.env.EMAIL,
+      to: process.env.EMAIL,
+      subject: 'Horaire Delta',
+      text: 'Nouvelle connexion!',
+    }
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error)
+      } else {
+        console.log('Email sent: ' + info.response)
+      }
+    })
+
     res.json({ authenticate: 'true' })
   } else {
     res.json({ authenticate: 'false' })
